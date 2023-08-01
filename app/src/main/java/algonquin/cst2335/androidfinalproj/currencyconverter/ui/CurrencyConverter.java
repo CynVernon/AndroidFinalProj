e378c9be33523edb2550f2896c94409d92e72b13
Cyn Vernon <vern0041@algonquinlive.com>
2023-08-01 14:49:59 -0400

    Added a bunch of French language support. Added Volley server Added instructions for the fragment about page Added the toolbar

--- app/src/main/java/algonquin/cst2335/androidfinalproj/currencyconverter/ui/CurrencyConverter.java
+++ app/src/main/java/algonquin/cst2335/androidfinalproj/currencyconverter/ui/CurrencyConverter.java
@@ -4,22 +4,37 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
 import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
+import java.util.Currency;
+import java.util.HashMap;
+import java.util.Map;
 
 import algonquin.cst2335.androidfinalproj.R;
+import algonquin.cst2335.androidfinalproj.aviationtracker.AviationActivity;
+import algonquin.cst2335.androidfinalproj.bearimages.BearActivity;
+import algonquin.cst2335.androidfinalproj.triviadatabase.WelcomeActivity;
 import algonquin.cst2335.androidfinalproj.currencyconverter.data.CurrencyConverterModel;
 import algonquin.cst2335.androidfinalproj.currencyconverter.data.ResultDetailsModel;
 import algonquin.cst2335.androidfinalproj.databinding.ActivityCurrencyConverterBinding;
-import algonquin.cst2335.androidfinalproj.currencyconverter.ui.ResultDetailsFragment;
 
 
 public class CurrencyConverter extends AppCompatActivity {
@@ -27,9 +42,6 @@ public class CurrencyConverter extends AppCompatActivity {
     private ActivityCurrencyConverterBinding variableBinding;
 
     protected RequestQueue queue = null;
-
-    ArrayList<Result> results = new ArrayList<>();
-
     Boolean hasBeenConverted = false;
 
     @Override
@@ -43,11 +55,19 @@ public class CurrencyConverter extends AppCompatActivity {
         variableBinding = ActivityCurrencyConverterBinding.inflate(getLayoutInflater());
         setContentView(variableBinding.getRoot());
 
+        //tool bar lol
+        setSupportActionBar(variableBinding.toolbar);
+
+        //volley queue
+        queue = Volley.newRequestQueue(this);
+
         //creating intent for results page
         Intent resultsPage = new Intent(CurrencyConverter.this, ResultsPage.class);
 
         //shared preferences
         SharedPreferences pref = getSharedPreferences("MyData", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = pref.edit();
+
 
         String currency = pref.getString("Currency", "");
         String amount = pref.getString("Amount", "");
@@ -80,38 +100,73 @@ public class CurrencyConverter extends AppCompatActivity {
         //on click listener for converting
         variableBinding.convertBtn.setOnClickListener( clk ->{
 
+
+            //getting all vars
+            String oldC = variableBinding.currencyEnter.getText().toString();
+            String newC = variableBinding.newCurrencyEnter.getText().toString();
+            String amountToC = variableBinding.amountEnter.getText().toString();
             //temporary holder for new amount
             String temp = "new amount";
+            String url = "";
 
             //verifying the data entered
             if(verifyData() == true ){
-                //shared preferences, putting the strings there
-                SharedPreferences.Editor editor = pref.edit();
-                editor.putString("Currency", variableBinding.currencyEnter.getText().toString());
-                editor.putString("Amount", variableBinding.amountEnter.getText().toString());
-                editor.putString("NewAmount", temp);
-                editor.putString("NewCurrency", variableBinding.newCurrencyEnter.getText().toString());
-               editor.apply();
-
-                resultsPage.putExtra("Currency", variableBinding.currencyEnter.getText().toString());
-                resultsPage.putExtra("Amount", variableBinding.amountEnter.getText().toString());
-                resultsPage.putExtra("NewAmount", temp);
-                resultsPage.putExtra("NewCurrency", variableBinding.newCurrencyEnter.getText().toString());
 
                //thing
                 setConverted(true);
 
-                //viewmodel for fragment?
-                ResultDetailsModel fragment = new ResultDetailsModel();
-                //fragment.setViewModel(model);
+                //API key: 15b1ecf20ebd3f7f75267fe3bdc61e2d53a081df
+                String key = "15b1ecf20ebd3f7f75267fe3bdc61e2d53a081df";
 
-                //loading the fragment
-                ResultDetailsFragment resultFragment = new ResultDetailsFragment();
-                FragmentManager fMgr = getSupportFragmentManager();
-                FragmentTransaction tx = fMgr.beginTransaction();
-                tx.replace(R.id.fragmentLocation, resultFragment);
-                tx.commit();
-                tx.addToBackStack("");
+                //https://api.getgeoapi.com/v2/currency/convert?format=json&from=AUD&to=CAD&amount=1&api_key=YOUR_API_KEY&format=json
+
+                //Must use a number in the JSON request to convert
+                double num = Double.parseDouble(amountToC);
+
+                //JSON Object things.
+                url = "https://api.getgeoapi.com/v2/currency/convert?format=json&from=" + oldC+ "&to="+ newC +"&amount=" + num +"&api_key="+ key + "&format=json";
+
+                JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null,
+                        (response) ->{
+                            Toast.makeText(this, "successful api things", Toast.LENGTH_SHORT).show();
+                            try{
+                                JSONObject ratesObject = response.getJSONObject("rates");
+                                JSONObject mainObject = ratesObject.getJSONObject(newC);
+
+                               String nA = mainObject.getString("rate_for_amount");
+
+                                //String cA = String.valueOf(convertedAmount);
+                                double amountToFormat = Double.parseDouble(nA);
+                                String formattedResult = String.format("%.2f", amountToFormat);
+
+                              variableBinding.result.setText(formattedResult);
+
+                              //adding to shared prefs
+                              editor.putString("NewAmount", formattedResult);
+                              resultsPage.putExtra("NewAmount", formattedResult);
+                              //editor.apply();
+
+                            } catch(JSONException e){
+
+                            }
+                        },
+                        (error) -> {
+                            Toast.makeText(this, "bad api things", Toast.LENGTH_SHORT).show();
+                        });
+                queue.add(request);
+
+                //shared preferences, putting the strings there
+                editor.putString("Currency", oldC);
+                editor.putString("Amount", amountToC);
+                // editor.putString("NewAmount", temp);
+                editor.putString("NewCurrency", newC);
+                editor.apply();
+
+                resultsPage.putExtra("Currency", variableBinding.currencyEnter.getText().toString());
+                resultsPage.putExtra("Amount", variableBinding.amountEnter.getText().toString());
+                //resultsPage.putExtra("NewAmount", temp);
+                resultsPage.putExtra("NewCurrency", variableBinding.newCurrencyEnter.getText().toString());
+
             }
             else {
 
@@ -132,22 +187,64 @@ public class CurrencyConverter extends AppCompatActivity {
     public boolean verifyData(){
 
         String currency = variableBinding.currencyEnter.getText().toString();
+        String amount = variableBinding.amountEnter.getText().toString();
+        String newCurrency = variableBinding.newCurrencyEnter.getText().toString();
 
-        if (currency.length() > 3) {
-            Toast.makeText(this, "Currency format: 3 Letters (ex. CAD, USD)",  Toast.LENGTH_SHORT).show();
+        if (currency.length() != 3){
+            Toast.makeText(this, "Please enter a valid amount",  Toast.LENGTH_SHORT).show();
             return false;
         }
-        else if (currency.length() == 0){
-            Toast.makeText(this, "Currency format: 3 Letters (ex. CAD, USD)",  Toast.LENGTH_SHORT).show();
+        else if (amount.length() == 0){
+            Toast.makeText(this, "Please enter a valid amount",  Toast.LENGTH_SHORT).show();
             return false;
-        } else if (currency.length() < 3 ){
-            Toast.makeText(this, "Currency format: 3 Letters (ex. CAD, USD)",  Toast.LENGTH_SHORT).show();
+        }
+        else if(newCurrency.length() != 3){
+            Toast.makeText(this, "Please enter a valid amount",  Toast.LENGTH_SHORT).show();
             return false;
-        } else{
+        }
+        else{
             return true;
         }
 
-        //todo: Verify new Currency format
-        //todo: verify number?
     } //end of verifyData()
+
+    //the menubar inflater
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.currency_menu_toolbar, menu);
+
+        return true;
+    }
+
+    //da actual menu bar
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        Intent bearPage = new Intent(CurrencyConverter.this, BearActivity.class);
+        Intent triviaPage = new Intent(CurrencyConverter.this, WelcomeActivity.class);
+        Intent aviationPage = new Intent(CurrencyConverter.this, AviationActivity.class);
+
+        if (item.getItemId() == R.id.triv_time) {
+            startActivity(triviaPage);
+        }
+        else if(item.getItemId() == R.id.avia_trac) {
+            startActivity(aviationPage);
+        }
+        else if(item.getItemId() == R.id.bear_imag){
+            startActivity(bearPage);
+        }
+        else if(item.getItemId() == R.id.action_help){
+
+            //loading the fragment
+            InstructionFragment resultFragment = new InstructionFragment();
+            FragmentManager fMgr = getSupportFragmentManager();
+            FragmentTransaction tx = fMgr.beginTransaction();
+            tx.replace(R.id.fragmentLocation, resultFragment);
+            tx.commit();
+            tx.addToBackStack("");
+        }
+
+        return true;
+    } //end of menu bar
+
 }
--- app/src/main/java/algonquin/cst2335/androidfinalproj/currencyconverter/ui/InstructionFragment.java
+++ app/src/main/java/algonquin/cst2335/androidfinalproj/currencyconverter/ui/InstructionFragment.java
@@ -1,9 +1,6 @@
 package algonquin.cst2335.androidfinalproj.currencyconverter.ui;
 
 
-import android.content.Context;
-import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,21 +8,17 @@ import android.view.ViewGroup;
 
 import androidx.fragment.app.Fragment;
 
-import algonquin.cst2335.androidfinalproj.databinding.ResultDetailsLayoutBinding;
+import algonquin.cst2335.androidfinalproj.databinding.InstructionFragmentLayoutBinding;
+
+public class InstructionFragment extends Fragment {
 
 
-public class ResultDetailsFragment extends Fragment {
-
-
-    public ResultDetailsFragment(){}
+    public InstructionFragment(){}
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         super.onCreateView(inflater, container, savedInstanceState);
-        ResultDetailsLayoutBinding binding = ResultDetailsLayoutBinding.inflate(inflater);
-
-        binding.resultView.setText("");
-          binding.amountView.setText("");
+        InstructionFragmentLayoutBinding binding = InstructionFragmentLayoutBinding.inflate(inflater);
 
         return binding.getRoot();
    } //end of oncreate()
